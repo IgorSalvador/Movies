6b9383ccfce98e1962d032cb1e761ede78d26761
Igor Henrique Salvador <igorsalvador0621@gmail.com>
2023-05-26 22:54:47 -0300

    Creating an API to work with an movie table
    Working with an API to do CRUD operations on MySQL Database and documenting API functions on swagger.

---
+++ MoviesApi/MoviesApi/Profiles/FilmeProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MoviesApi.Data.Dtos;
+using MoviesApi.Models;
+
+namespace MoviesApi.Profiles
+{
+    public class FilmeProfile : Profile
+    {
+        public FilmeProfile()
+        {
+            CreateMap<CreateFilmeDto, Filme>();
+            CreateMap<UpdateFilmeDto, Filme>();
+            CreateMap<Filme, UpdateFilmeDto>();
+            CreateMap<Filme, ReadFilmeDto>();
+        }
+    }
+}
